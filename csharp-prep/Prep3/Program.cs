cbcd64a6bda1de2d1f46bc60396fa77b6c54f89a
Heiner000 <garrett.heiner@gmail.com>
2024-04-27 20:11:07 -0700

    adds prep 3.1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Xml;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Console.Write("What is the magic number? ");
+        string userInput = Console.ReadLine();
+        int magicNumber = int.Parse(userInput);
+
+        Console.Write("What is your guess? ");
+        string userGuess = Console.ReadLine();
+        int guess = int.Parse(userGuess);
+
+        if (magicNumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (magicNumber < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        
     }
 }
