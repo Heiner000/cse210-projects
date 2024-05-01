185de52ae4a093c4e1a9b9d038e4a65b912b35c2
Heiner000 <garrett.heiner@gmail.com>
2024-04-30 22:58:37 -0700

    finishes w2 learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // job1.Display();
+
+
+        Job job2 = new Job();
+        job2._jobTitle = "Salt Pusher";
+        job2._company = "Salt Works";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        // job2.Display();
+
+        // Console.WriteLine(job1._company);
+        // Console.WriteLine(job2._company);
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Garrett Heiner";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Console.WriteLine(myResume._jobs[1]._jobTitle);
+
+        myResume.Display();
+
     }
 }
