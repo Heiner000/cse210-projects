437a086d3580bd1b7318ecac8fc71e4719a0ff60
Heiner000 <garrett.heiner@gmail.com>
2024-05-27 23:14:53 -0700

    defines classes in foundation 2 activity

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Versioning;
+
+public class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(List<Product> products, Customer customer)
+    {
+        _products = products;
+        _customer = customer;
+    }
+
+    public double GetTotalCostPlusShipping()
+    {
+        double totalCost = 0;
+
+        foreach (Product product in _products)
+        {
+            totalCost += product.GetTotalCost();
+        }
+
+        double shippingCost = _customer.isInUSA() ? 5.00 : 35.00;
+        totalCost += shippingCost;
+
+        return totalCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        string packingLabel = "Packing List:\n";
+        foreach (Product product in _products)
+        {
+            packingLabel += $"{product.GetName()} : {product.GetProductID()}";
+        }
+        return packingLabel;
+    }
+}
